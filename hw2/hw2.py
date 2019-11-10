7d0ba6b7b19cff64ca2c736c21a8e156bc6d2dd9
Ramtin <ramtinj@kth.se>
2019-11-10 19:29:23 +0100

    kinda complex code, came up with a new solution, just filter items in basket against singletons and go through candidate pairs with this new list and check if the frequent_items list contains two elements from this new short list

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -4,13 +4,26 @@ import numpy as np
 
 item_count = {}
 
+
 class CandidatePair:
     def __init__(self, frequent_items):
         self.frequent_items = frequent_items
-    
+        self.nr_occurrences = 0
+        self.nr_items_matched_in_basket = 0
+
     def __eq__(self, candidate_pair_obj):
-        is_equal = self.compare_lists(self.frequent_items, candidate_pair_obj.frequent_items)
+        is_equal = self.compare_lists(
+            self.frequent_items, candidate_pair_obj.frequent_items)
         return is_equal
+
+    def __str__(self):
+        return self.frequent_items
+    '''
+    this is just for testing now have to write a proper method for this soon
+    '''
+
+    def __repr__(self):
+        return "<Test a:%s b:%s>" % (self.frequent_items[0], self.frequent_items[1])
 
     def compare_lists(self, candidate_pair_list):
         check_len = len(self.frequent_items)
@@ -21,12 +34,20 @@ class CandidatePair:
         else:
             return False
 
+    def one_match_found(self):
+        self.nr_items_matched_in_basket += 1
+
+    def is_full_match(self):
+        if self.nr_items_matched_in_basket == len(self.frequent_items):
+            self.nr
+
 def read_data():
     nr_transactions = 0
     with open('dataset.txt') as inputfile:
         for line in inputfile:
             nr_transactions += 1
-            temp_arr = [int(x) for x in line.strip().split(' ')]  # changes from str to in to save memory
+            # changes from str to in to save memory
+            temp_arr = [int(x) for x in line.strip().split(' ')]
             for item in temp_arr:
                 if item not in item_count.keys():
                     item_count[item] = 1
@@ -43,13 +64,42 @@ def find_singletons(support_threshold):
     return singletons
 
 
+def generate_candidate_pairs(singletons, k_itemset_list=None):
+    singletons = list(singletons)
+    if k_itemset_list == None:
+        dubbeltons = []
+        for i in range(len(singletons)):
+            for j in range(i+1, len(singletons)):
+                candidate_pair = CandidatePair([singletons[i], singletons[j]])
+                dubbeltons.append(candidate_pair)
+        return dubbeltons
+    else:
+        return 0
+
+
+def ith_filter(candidate_pair_list, singletons, support_threshold):
+    k_itemset_count = []
+    k_pair = len(candidate_pair_list[0].frequent_items)
+    with open('dataset.txt') as inputfile:
+        for line in inputfile:
+            basket = [int(x) for x in line.strip().split(' ')]
+            matched_items_candidate_pair = defaultdict(list)
+            for item in basket:
+                if item in singletons:
+                    for candidate_pair in candidate_pair_list:
+                        if item in candidate_pair.frequent_items:
+                            matched_items_candidate_pair[item].append(candidate_pair)
+
+
 def main():
     nr_transactions = read_data()
     print(len(item_count.keys()))
-    s = nr_transactions / 100 # 1 % of baskets/number of transactions
+    s = nr_transactions / 100  # 1 % of baskets/number of transactions
     singletons = find_singletons(s)
     print(len(singletons))
     print(singletons.get(25))
+    dubletons = generate_candidate_pairs(singletons)
+    print(dubletons)
 
 
 main()
