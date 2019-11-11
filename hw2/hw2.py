bd348b84d019078ad3f62801294c1c48f0fb4d3d
Ramtin <ramtinj@kth.se>
2019-11-11 14:46:27 +0100

    the new clearner faster approach

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,37 +1,68 @@
+from collections import defaultdict
 import numpy as np
-
-def import_dataset():
-    dataset = []
-    with open('dataset.txt') as inputfile:
-        for line in inputfile:
-            dataset.append(line.strip().split(' '))
-        return dataset
-
-def get_unique_items(dataset):
-    dataset = np.hstack(np.array(dataset))
-    only_unique_items = np.unique(dataset)
-    return only_unique_items
-
-def build_occurrence_dict(uniqe_items, dataset):
-    item_nr_occurrences = {}
-    for item in uniqe_items:
-        nr_occurrences = 0
-        for basket in dataset:
-            if item in basket:
-                nr_occurrences += 1 
-        item_nr_occurrences[item] = nr_occurrences
-    return item_nr_occurrences
-
-def main():
-    dataset = import_dataset()
-    unique_items = get_unique_items(dataset)
-    print()
-    print("number of baskets", len(dataset))
-    print()
-    print(unique_items)
-    print("number of unique items", len(unique_items))
-    item_nr_occurrences = build_occurrence_dict(unique_items,dataset)
-    print("dict length", len(item_nr_occurrences))
+Lk_arr = []
+s_threshold = 4000
+data_file_path = “./sales.dat”
 
 
-main()
+def Read_Data_C0():
+    C0 = defaultdict()
+    data_file = open(data_file_path)
+    for line in data_file:
+        basket_items = set(map(int, line.split()))
+        for item in basket_items:
+            if item in C0.keys():
+                C0[item] = C0[item] + 1
+            else:
+                C0[item] = 1
+    data_file.close()
+    return C0
+
+
+def Count_Occurences_Of_Itemsets(Ck):
+    data_file = open(data_file_path)
+    for line in data_file:
+        basket_items = set(map(int, line.split()))
+        for itemset in Ck.keys():
+            itemset_in_basket = True
+            for item in itemset:
+                if item not in basket_items:
+                    itemset_in_basket = False
+                    break
+            if itemset_in_basket == True:
+                Ck[itemset] = Ck[itemset] + 1
+    data_file.close()
+    return Ck
+
+
+def Filter_By_S_Threshold(Ck):
+    Lk = defaultdict()
+    for itemset in Ck.keys():
+        if Ck[itemset] >= s_threshold:
+            Lk[itemset] = Ck[itemset]
+    return Lk
+
+
+def Generate_CkPlus1(L0, Lk):
+    return 0
+
+
+def Create_Frequent_Itemsets(C0):
+    return 0
+
+
+def Print_Empty_Lines(n_lines):
+    for a in range(n_lines):
+        print(“”)
+
+
+def Main():
+    C0 = Read_Data_C0()
+    L0 = Filter_By_S_Threshold(C0)
+    print(L0)
+    Print_Empty_Lines(3)
+    print(len(L0))
+    print(len(C0.keys()))
+
+
+Main()
