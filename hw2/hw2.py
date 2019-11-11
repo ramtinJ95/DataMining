9d3f793c704e0721042fd2b51996cab7036a1aa1
Ramtin <ramtinj@kth.se>
2019-11-11 16:21:09 +0100

    implementation of first part done using lecture method

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,8 +1,9 @@
 from collections import defaultdict
 import numpy as np
+
 Lk_arr = []
-s_threshold = 4000
-data_file_path = “./sales.dat”
+s_threshold = 2000
+data_file_path = "./dataset.txt"
 
 
 def Read_Data_C0():
@@ -44,25 +45,52 @@ def Filter_By_S_Threshold(Ck):
 
 
 def Generate_CkPlus1(L0, Lk):
-    return 0
+    Ckp1 = defaultdict()
+    Ckp1Temp = defaultdict()
+    for itemset in Lk.keys():
+        for item in L0.keys():
+            if type(itemset) is int:
+                Ckp1Temp[itemset, item] = 0
+            else:
+                Ckp1Temp[itemset + (item, )] = 0
+    for itemset in Ckp1Temp.keys():
+        items_in_order = True
+        for item_index in range(len(itemset) - 1):
+            if itemset[item_index] >= itemset[item_index + 1]:
+                items_in_order = False
+                break
+        if items_in_order == True:
+            Ckp1[itemset] = Ckp1Temp[itemset]
+    return Ckp1
 
 
 def Create_Frequent_Itemsets(C0):
+    k = 1
+    L0 = Filter_By_S_Threshold(C0)
+    Lk_arr.append(L0)
+    print(L0)
+    Print_Empty_Lines(3)
+    while True:
+        Ckp1 = Generate_CkPlus1(Lk_arr[0], Lk_arr[k - 1])
+        Ckp1 = Count_Occurences_Of_Itemsets(Ckp1)
+        Lkp1 = Filter_By_S_Threshold(Ckp1)
+        if len(Lkp1) <= 0:
+            break
+        Lk_arr.append(Lkp1)
+        print(Lkp1)
+        Print_Empty_Lines(3)
+        k = k + 1
     return 0
 
 
 def Print_Empty_Lines(n_lines):
     for a in range(n_lines):
-        print(“”)
+        print("")
 
 
 def Main():
     C0 = Read_Data_C0()
-    L0 = Filter_By_S_Threshold(C0)
-    print(L0)
-    Print_Empty_Lines(3)
-    print(len(L0))
-    print(len(C0.keys()))
+    Create_Frequent_Itemsets(C0)
 
 
 Main()
