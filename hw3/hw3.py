a5a87e3b0306666350a7ebda5e482954d1fe8b14
Ramtin <ramtinj@kth.se>
2019-11-17 22:47:09 +0100

    i think lab3 is done now

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -76,14 +76,83 @@ class Triest_basic:
             eps = 1
         return self.tau * eps
 
+class Triest_impr:
+    def __init__(self, S_size):
+        self.time = 0 # t in the paper
+        self.tau = 0
+        self.edge_sampled = set()
+        self.S_size = S_size
+        self.counters = {}
+
+    def sample_edge(self, edge):
+        if self.time <= self.S_size:
+            return True
+        if random.random() <= (self.S_size / self.time):
+            edge_to_remove = random.sample(self.edge_sampled,1)[0]
+            self.edge_sampled.remove(edge_to_remove)
+            return True
+        return False
+    
+    def update_counters(self, edge):
+        s1 = set()
+        s2 = set()
+        for sample_edge in self.edge_sampled:
+            if sample_edge.frm == edge.frm:
+                s1.add(sample_edge.to)
+            if sample_edge.to == edge.frm:
+                s1.add(sample_edge.frm)
+            if sample_edge.frm == edge.to:
+                s2.add(sample_edge.to)
+            if sample_edge.to == edge.to:
+                s2.add(sample_edge.frm)
+        eta = ((self.time - 1) * (self.time - 2)) / (self.S_size * (self.S_size - 1))
+        if eta < 1:
+            eta = 1
+        for c in (s1 & s2):
+            self.tau += eta
+            self.counters[c] = self.counters.get(c, 0) + eta
+            self.counters[edge.frm] = self.counters.get(edge.frm, 0) + eta
+            self.counters[edge.to] = self.counters.get(edge.to, 0) + eta
+    
+    def run(self, stream_edges):
+        for edge in stream_edges:
+            if self.time % 1000 == 0:
+                print ("element: ", self.time, "value: ", self.tau)
+            self.time += 1
+            self.update_counters(edge)
+            if self.sample_edge(edge):
+                self.edge_sampled.add(edge)
+        return self.tau
+
+
+
 edges = set()
 test_set = set()
 
 def read_data():
-    with open('dataset.txt') as f:
+    with open('facebookDataset.txt') as f:
         for line in f:
             if line[0] == "%":
                 continue
             data = line.split()
             if data[0] != data[1]:
                 test_set.add(Edge(data[0], data[1]))
+
+def main():
+    run_base_triest = False
+    if run_base_triest:
+        S_size = 1000
+        read_data()
+        t = Triest_basic(S_size)
+        expected = t.run(test_set)
+        true_value = Triest_basic(len(test_set)).run(test_set)
+        print("With ", S_size, "samples the expected value is ", expected," . The true value is ", true_value," . Error: ", abs(true_value - expected)," triangles")
+    else:
+        S_size = 1000
+        read_data()
+        t = Triest_impr(S_size)
+        expected = t.run(test_set)
+        true_value = Triest_impr(len(test_set)).run(test_set)
+        print("With ", S_size, "samples the expected value is ", expected," . The true value is ", true_value," . Error: ", abs(true_value - expected)," triangles")
+
+main()
