70b360476493539ed81ed9a868e625eea096a6f3
Ramtin <ramtinj@kth.se>
2019-11-17 13:59:23 +0100

    treist base is done i think i havent done any testing yet

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,3 +1,5 @@
+import random
+
 class Edge:
     def __init__(self, frm, to):
         if frm < to:
@@ -14,12 +16,65 @@ class Edge:
         return isinstance(self, type(other)) and other.frm == self.frm and self.to == other.to
 
 class Triest_basic:
-    def __init__(self, S_size): # S_size = M
-        self.t = 0 # time
+    def __init__(self, S_size): # S_size is M in the paper
+        self.time = 0 # t in the paper
         self.tau = 0
         self.edge_sampled = set()
         self.S_size = S_size
         self.counters = {}
+    
+    def sample_edge(self, edge):
+        if self.time <= self.S_size:
+            return True
+        if random.random() <= (self.S_size / self.time):
+            edge_to_remove = random.sample(self.edge_sampled,1)[0]
+            self.edge_sampled.remove(edge_to_remove)
+            self.update_counters('-', edge)
+            return True
+        return False
+    
+    def update_counters(self, op, edge):
+        s1 = set()
+        s2 = set()
+        for sample_edge in self.edge_sampled:
+            if sample_edge.frm == edge.frm:
+                s1.add(sample_edge.to)
+            if sample_edge.to == edge.frm:
+                s1.add(sample_edge.frm)
+            if sample_edge.frm == edge.to:
+                s2.add(sample_edge.to)
+            if sample_edge.to == edge.to:
+                s2.add(sample_edge.frm)
+        for c in (s1 & s2):
+            if op == '+':
+                self.tau += 1
+                self.counters[c] = self.counters.get(c, 0) + 1
+                self.counters[edge.frm] = self.counters.get(edge.frm, 0) + 1
+                self.counters[edge.to] = self.counters.get(edge.to, 0) + 1
+            elif op == '-':
+                self.tau -= 1
+                self.counters[c] = self.counters.get(c, 0) - 1
+                if self.counters[c] <= 0:
+                    del self.counters[c]
+                self.counters[edge.frm] = self.counters.get(edge.frm, 0) - 1
+                if self.counters[edge.frm] <= 0:
+                    del self.counters[edge.frm]
+                self.counters[edge.to] = self.counters.get(edge.to, 0) - 1
+                if self.counters[edge.to] <= 0:
+                    del self.counters[edge.to]
+    
+    def run(self, stream_edges):
+        for edge in stream_edges:
+            if self.time % 1000 == 0:
+                print ("element: ", self.time, "value: ", self.tau)
+            self.time += 1
+            if self.sample_edge(edge):
+                self.edge_sampled.add(edge)
+                self.update_counters('+', edge)
+        eps = (self.time *(self.time - 1)* (self.time - 2)) / (self.S_size * (self.S_size - 1) * (self.S_size - 2))
+        if eps < 1:
+            eps = 1
+        return self.tau * eps
 
 edges = set()
 test_set = set()
